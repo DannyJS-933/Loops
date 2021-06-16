205eb3da2c8decbb3873ca3c6e97cafa44864c4e
DannyJS-933 <danny.933@hotmail.com>
2021-06-15 18:29:38 -0600

    first commit

---
+++ Logic.js
@@ -0,0 +1,22 @@
+
+/*for (var i = 0; i < 6; i++) {
+    
+    console.log(i);
+}*/
+
+/*let i = 0;
+do {
+    
+    i = i + 2;
+    console.log("Numero: " + i); 
+
+} while (i < 10);*/
+
+
+let i = 1; 
+
+while (i < 20) {
+
+    i = i * 3;
+    console.log("Resultado: " + i);
+}
